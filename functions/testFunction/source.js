1eab84de32fe3d7a10029dd60b152ad946794fdc
Randy Otero Jimenez <rotero@espinolabs.fr>
2019-07-30 18:09:58 +0200

    Update deploy

--- functions/testFunction/source.js
+++ functions/testFunction/source.js
@@ -13,5 +13,5 @@ exports = function(arg){
     Try running in the console below.
   */
   
-  return {arg: arg, data: {message: "this is sparta and my sons"}};
+  return {arg: arg, data: {message: "this is sparta and my sons as"}};
 };
